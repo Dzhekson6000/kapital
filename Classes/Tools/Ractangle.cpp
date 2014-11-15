3d4c6647ad070e5363d331371e23a4f88a1fd1f1
Dzhekson6000 <Dzhekson6000@yandex.ru>
2014-11-15 06:04:16 +0300

    Add Rectangle

---
+++ Classes/Tools/Ractangle.cpp
@@ -0,0 +1,1 @@
+#include "Ractangle.h"
---
+++ Classes/Tools/Ractangle.h
@@ -0,0 +1,16 @@
+#ifndef RACTANGLE_H_
+#define RACTANGLE_H_
+
+class Ractangle
+{
+private:
+	//изометрические координаты
+	float _x;
+	float _y;
+
+public:
+	Ractangle(){};
+	Ractangle(float x, float y);
+};
+
+#endif // RACTANGLE_H_
