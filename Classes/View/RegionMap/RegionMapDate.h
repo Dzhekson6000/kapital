038a69912c41b1095d5b6af8abfbf1ec8db5becf
Dzhekson6000 <Dzhekson6000@yandex.ru>
2014-11-15 06:03:27 +0300

    Add RegionMapDate

--- Classes/View/RegionMap/RegionMapDate.h
+++ Classes/View/RegionMap/RegionMapDate.h
@@ -2,11 +2,13 @@
 #define REGION_MAP_DATE_H_
 
 #include "RegionMapObject.h"
+#include "FonRegionMap.h"
 #include "Tools/List.h"
 
 class RegionMapDate{
 public:
 	List<RegionMapObject*>* objects;
+	FonRegionMap* _fon;
 	~RegionMapDate(){
 		delete objects;
 	}
